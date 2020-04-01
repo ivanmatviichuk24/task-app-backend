4e89c0a48548a1b81681e25465432c21a446667f
Іван Матвійчук <ivanmatviichuk24@gmail.com>
2020-04-01 16:21:11 +0300

    delete comented code

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const io = socketio(server);
 const cors = require("cors");
 
 app.io = io;
-app.pussy = "pussy";
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 const port = process.env.PORT || 5000;
@@ -18,22 +17,6 @@ app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-/*app.use(async (req, res, next) => {
-  if (req.user) {
-    const tasks = await req.user
-      .populate({
-        path: "tasks",
-        options: {
-          sort: "-updatedAt"
-        }
-      })
-      .execPopulate();
-    req.app.io.to(`${req.user.email}`).emit("tasks", req.user.tasks);
-  }
-
-  next();
-});*/
 
 io.on("connection", socket => {
   app.socket = socket;
