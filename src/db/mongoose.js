2914d1252cc8a6f8e07a1308d1d14235914a18e9
Іван Матвійчук <ivanmatviichuk24@gmail.com>
2020-04-01 16:27:13 +0300

    resolve issue

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
+const url = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/";
 
 mongoose.connect(`${url}heroku_bx162mvm`, {
   useNewUrlParser: true,
