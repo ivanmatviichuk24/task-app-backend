26f530e2dd7d392488604c8b4dbf9e1bac73bf81
Іван Матвійчук <ivanmatviichuk24@gmail.com>
2020-04-01 16:59:30 +0300

    cors policy

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.io = io;
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
